c17d1601b2aa4c1bd382b56c3b9c12685d21c226
dyoshikawa <yoshikawada@crunchtimer.jp>
2019-04-08 14:56:19 +0900

    fix: abc007_3

--- abc007_3/main.py
+++ abc007_3/main.py
@@ -16,7 +16,6 @@ def filter_space_list(strs):
 
 def create_graph(strs):
     space_list = filter_space_list(strs)
-    print(space_list)
     graph = {}
     for space in space_list:
         graph[space] = [
@@ -27,41 +26,58 @@ def create_graph(strs):
                 (space[0], space[1]-1),
             ] if x in space_list
         ]
-    # print(graph)
     return graph
+
+
+def exists_searched_list(searched_list, space):
+    for item in searched_list:
+        if item[0] == space:
+            return True
+    return False
+
+
+def get_searced_list_by_list(searched_list, space_list):
+    for space in space_list:
+        for searched in searched_list:
+            if searched[0] == space:
+                return searched
+    return (None, 0)
 
 
 def main(input_list):
     s_list = list(map(int, input_list[0][1].split()))
     sy = s_list[0]
     sx = s_list[1]
+    start = (sy, sx)
     g_list = list(map(int, input_list[0][2].split()))
     gy = g_list[0]
     gx = g_list[1]
+    goal = (gy, gx)
     graph = create_graph(input_list[1])
 
     search_queue = deque()
-    search_queue += graph[(sy, sx)]
-
+    search_queue += graph[start]
     searched_list = []
-    count = 0
 
     while search_queue:
-        target = search_queue.popleft()
-        if target == (gy, gx):
-            print('goal')
+        space = search_queue.popleft()
+
+        if not exists_searched_list(searched_list, space):
+            search_queue += graph[space]
+            space_old = get_searced_list_by_list(searched_list, graph[space])
+            searched_list.append((space, space_old[1]+1))
+
+        if space == goal:
             break
 
-        if not target in searched_list:
-            search_queue += graph[target]
-            searched_list.append(target)
-            count += 1
-
-    return count
+    return searched_list[-1][1]
 
 
 if __name__ == "__main__":
-    input_nums = []
-    for _ in range(5):
-        input_nums.append(int(input()))
-    print(main(input_nums))
+    input_list = []
+    input_list.append([input(), input(), input()])
+    input_strs = []
+    for _ in range(int(input_list[0][0].split()[0])):
+        input_strs.append(input())
+    input_list.append(input_strs)
+    print(main(input_list))

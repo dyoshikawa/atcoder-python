30fd5bcd5e5deae8abd2479e4cae8457355e7236
dyoshikawa <yoshikawada@crunchtimer.jp>
2019-04-10 13:54:13 +0900

    fix: abc051_d

--- abc051_d/main.py
+++ abc051_d/main.py
@@ -23,9 +23,14 @@ def get_lowest_cost_vertex(costs, processed):
     return lowest_cost_node
 
 
-def get_best_route(graph, start, goal):
+def get_best_routes(graph, start):
     costs = {}
     parents = {}
+    processed = []
+
+    costs[start] = 0
+    parents[start] = None
+    processed.append(start)
 
     for item in graph[start].keys():
         costs[item] = graph[start][item]
@@ -36,50 +41,68 @@ def get_best_route(graph, start, goal):
             costs[item] = float("inf")
             parents[item] = None
 
-    # print(costs)
-    # print(parents)
-    processed = []
-    print("start")
-    print(start)
-
     node = get_lowest_cost_vertex(costs, processed)
-    print("最も近いnode")
-    print(node)
+
     while node is not None:
         cost = costs[node]
         neighbors = graph[node]
 
         for n in neighbors.keys():
             new_cost = cost + neighbors[n]
-            print(costs)
-            print(n)
             if costs[n] > new_cost:
                 costs[n] = new_cost
                 parents[n] = node
-            processed.append(node)
-            node = get_lowest_cost_vertex(costs, processed)
+        processed.append(node)
+        node = get_lowest_cost_vertex(costs, processed)
 
-    print(parents)
-    return ""
+    routes = []
+    for item in graph.keys():
+        if item != start:
+            route = []
+            route.append(item)
+            parent = parents[item]
+            while 1:
+                route.append(parent)
+                if parent == start:
+                    break
+                parent = parents[parent]
+            route.reverse()
+            routes.append(route)
+    return routes
 
 
 def main(vertex_count, input_list):
-    print(input_list)
     graph = gen_graph(input_list)
-    print(graph)
 
+    routes = []
     for start in range(1, vertex_count+1):
-        for goal in range(1, vertex_count+1):
-            route = get_best_route(graph, start, goal)
-            # print(route)
+        for route in get_best_routes(graph, start):
+            routes.append(route)
 
-    return input_list[0][0]
+    route_strs = []
+    for route in routes:
+        route_strs.append(''.join(list(map(str, route))))
+
+    side_strs = []
+    for item in input_list:
+        side_strs.append(str(item[0]) + str(item[1]))
+        # side_strs.append(str(item[1]) + str(item[0]))
+
+    count = 0
+    route_strs_joined = '/'.join(route_strs)
+
+    for side_str in side_strs:
+        if route_strs_joined.find(side_str) == -1:
+            count += 1
+
+    return count
 
 
 if __name__ == "__main__":
-    vertex_count, sides_count = list(map(int, input().split()))
+    input_1 = input()
+    vertex_count, sides_count = list(map(int, input_1.split()))
     input_list = []
-    for _ in sides_count:
+    for _ in range(sides_count):
         vertex_1, vertex_2, distance = list(map(int, input().split()))
         input_list.append((vertex_1, vertex_2, distance))
     print(main(vertex_count, input_list))
